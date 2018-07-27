573c6e6c51bab568dd4ebbdb1dc8b50ab107631d
ck0903 <ck@hzgosun.com>
2018-07-27 09:13:43 +0800

    修改特征值的判断

--- Common-jni/src/main/java/com/hzgc/jni/FaceFunction.java
+++ Common-jni/src/main/java/com/hzgc/jni/FaceFunction.java
@@ -185,7 +185,7 @@ public class FaceFunction {
      * @return 返回float[]类型的特征值
      */
     public static float[] string2floatArray(String feature) {
-        if (feature != null && feature.length() == 512) {
+        if (feature != null && feature.length() > 0) {
             float[] featureFloat = new float[512];
             String[] strArr = feature.split(SPLIT);
             for (int i = 0; i < strArr.length; i++) {
