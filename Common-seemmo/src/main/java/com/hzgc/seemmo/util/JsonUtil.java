a24809d17ce42bc65a12c455d65185e555b9040e
test <121921013@qq.com>
2018-09-03 11:52:11 +0800

    'update'

--- Common-seemmo/src/main/java/com/hzgc/seemmo/util/JsonUtil.java
+++ Common-seemmo/src/main/java/com/hzgc/seemmo/util/JsonUtil.java
@@ -4,11 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.hzgc.seemmo.bean.ImageInfo;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
 public class JsonUtil {
 
     //imageInfo对象转字符串
